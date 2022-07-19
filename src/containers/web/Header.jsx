0f28327ddb85756c9fa784b8cd251ed8f4b9cf55
Aniisah Eka Rahmawati <aniisaher1@gmail.com>
2022-07-19 11:28:11 +0700

    update image

--- src/containers/web/Header.jsx
+++ src/containers/web/Header.jsx
@@ -137,8 +137,8 @@ const Header = ({ active, title, }) => {
                     <Box display={{ xs : "none", md : 'flex'}} alignItems='center' position={"absolute"} justifySelf="start" left="8rem">
                         <Link onClick={()=>navigate("/")}>
                             <Box component='img'
-                                src='/images/logo.png'
-                                alt='logo'
+                                src='/images/Logo.png'
+                                alt='Logo'
                                 mr={2}
                             />
                         </Link>
